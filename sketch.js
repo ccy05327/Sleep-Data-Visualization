c2467267bc9958f90defa3d202fef5145bab04c6
waystolearn <67034552+waystolearn@users.noreply.github.com>
2021-04-03 11:31:40 +0800

    Update sketch.js
    4/3 sleep

--- sketch.js
+++ sketch.js
@@ -45,7 +45,7 @@ function setup() {
   record.push(new Sleep('3/31', 2.85, 10.55, false));
   record.push(new Sleep('4/1', 3.25, 7.6, false));
   record.push(new Sleep('4/2', 2.98, 9.26, false));
-  // record.push(new Sleep('4/3', 23, 8, true));
+  record.push(new Sleep('4/3', 2.6, 8.66, false));
   // record.push(new Sleep('4/4', 23, 8, true));
 }
 
