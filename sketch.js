777fe745e1a5d219e81428a2ca4e881ec715ad08
Cynthia <67034552+ccy05327@users.noreply.github.com>
2021-04-23 09:48:09 +0800

    Update sketch.js
    4/23 sleep

--- sketch.js
+++ sketch.js
@@ -64,8 +64,8 @@ function setup() {
   record.push(new Sleep('4/19', 19.66, 10.86, false));
   record.push(new Sleep('4/20', 20.4, 3.75, true));
   record.push(new Sleep('4/21', 19.68, 11.63, false));
-//   record.push(new Sleep('4/22', 14.75, 7.08, false));
-//   record.push(new Sleep('4/23', 13, 3, true));
+  record.push(new Sleep('4/22'));
+  record.push(new Sleep('4/23', 1.06, 8.36, false));
 //   record.push(new Sleep('4/24', 0.03, 16.25, false));
 //   record.push(new Sleep('4/25', 13.25, 10.58, false));
 //   record.push(new Sleep('4/26', 17.66, 8.41, false));
