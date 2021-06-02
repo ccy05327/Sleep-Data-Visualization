ef6fed31a1463b6926ffb70c1d19a8eb4d602781
Cynthia <67034552+ccy05327@users.noreply.github.com>
2021-06-02 08:58:21 +0800

    Update sketch.js
    print may records

--- sketch.js
+++ sketch.js
@@ -22,7 +22,39 @@ function setup() {
   let canvasHeight = 375 + 28*31;
   createCanvas(1050, canvasHeight);
 
-  record.push(new Sleep('6/1', 14.41, 10.13, false));
+  record.push(new Sleep('5/1', 11.76, 6.53, false));
+record.push(new Sleep('5/2', 16.7, 6.86, false));
+record.push(new Sleep('5/3', 15.28, 12.86, false));
+record.push(new Sleep('5/4', 20.7, 6.93, false));
+record.push(new Sleep('5/5', 19.26, 11.38, false));
+record.push(new Sleep('5/6', 23.63, 10.15, false));
+record.push(new Sleep('5/7'));
+record.push(new Sleep('5/8', 1.65, 7.75, false));
+record.push(new Sleep('5/9', 3.61, 11.45, false));
+record.push(new Sleep('5/10', 1.33, 7.51, false));
+record.push(new Sleep('5/11', 1.48, 10.51, false));
+record.push(new Sleep('5/12', 3.86, 7.6, false));
+record.push(new Sleep('5/13', 4.25, 11.05, false));
+record.push(new Sleep('5/14', 8.16, 9.83, false));
+record.push(new Sleep('5/15', 10.5, 8.38, false));
+record.push(new Sleep('5/16', 12.08, 6.5, false));
+record.push(new Sleep('5/17', 9.88, 8.2, false));
+record.push(new Sleep('5/18', 11.5, 11.46, false));
+record.push(new Sleep('5/19', 18.16, 5.1, false));
+record.push(new Sleep('5/20', 1.83, 5.65, false));
+record.push(new Sleep('5/20', 21.85, 6.25, false)); 
+record.push(new Sleep('5/21', 18.01, 11.41, false));
+record.push(new Sleep('5/22', 21.83, 8.31, false));
+record.push(new Sleep('5/23', 22.73, 9.41, false));
+record.push(new Sleep('5/24', 21.41, 8.63, false));
+record.push(new Sleep('5/25'));
+record.push(new Sleep('5/26', 0.86, 11.71, false));
+record.push(new Sleep('5/27', 4.85, 8.66, false));
+record.push(new Sleep('5/28', 4.51, 9.45, false));
+record.push(new Sleep('5/29', 7.05, 10.86, false));
+record.push(new Sleep('5/30', 9.25, 8.85, false));
+record.push(new Sleep('5/31', 1.16, 8.66, false));
+  // record.push(new Sleep('6/1', 14.41, 10.13, false));
   // record.push(new Sleep('6/2', 16.7, 6.86, false));
   // record.push(new Sleep('6/3', 15.28, 12.86, false));
   // record.push(new Sleep('6/4', 20.7, 6.93, false));
@@ -104,12 +136,6 @@ function draw() {
       if(record[i].date === record[i-1].date){ // if two same date entries
         sameDay++;
         line(record[i].sleepTimeX, 124+(i-sameDay)*28, record[i].newAwake, 124+(i-sameDay)*28);
-        if (record[i].nextDay){
-          let nextDayX = map(record[i].nextDay, 0, 24, xpos, 980);
-          line(xpos+2, 126+(i+1-sameDay)*28, nextDayX, 126+(i+1-sameDay)*28);
-        }
-      } else if (sameDay > 0){
-        line(record[i-sameDay].sleepTimeX, 124+i*28, record[i-sameDay].awakeX, 124+i*28);
         if (record[i].nextDay){
           let nextDayX = map(record[i].nextDay, 0, 24, xpos, 980);
           line(xpos+2, 126+(i+1-sameDay)*28, nextDayX, 126+(i+1-sameDay)*28);
