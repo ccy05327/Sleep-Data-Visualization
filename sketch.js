574b7a5029f3ed4d22c157b12f5e617f242538e8
Cynthia <67034552+ccy05327@users.noreply.github.com>
2021-06-08 11:24:30 +0800

    Update sketch.js
    6/8 sleep (long dream and constant awake)

--- sketch.js
+++ sketch.js
@@ -16,6 +16,9 @@ function setup() {
   sleep.addRecord(2021, 6, 4, 17, 50, 2, 50, false); 
   sleep.addRecord(2021, 6, 5, 21, 7, 7, 34, false); 
   sleep.addRecord(2021, 6, 6, 22, 52, 8, 42, false); 
+  sleep.addRecord(2021, 6, 7, false); 
+  sleep.addRecord(2021, 6, 8, 1, 52, 9, 8, false); 
+
 
   
   console.log(sleep);
