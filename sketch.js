72bf764af72d91bb9d2f259f1dfb0a1f7880977b
Cynthia <explorationyear2020@gmail.com>
2022-01-17 09:10:57 +0800

    Update sketch.js
    1/16 sleep

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
   sleep.addRecord(2022, 1, 13, 19, 34, 5, 42, false);
   sleep.addRecord(2022, 1, 14, 22, 46, 6, 52, false);
   sleep.addRecord(2022, 1, 15, 22, 46, 6, 52, false);
-  // sleep.addRecord(2022, 1, 16, 22, 7, 5, 37, false);
+  sleep.addRecord(2022, 1, 16, 22, 39, 6, 57, false);
   // sleep.addRecord(2022, 1, 17, 23, 29, 7, 28, true);
   // sleep.addRecord(2022, 1, 18, 20, 54, 8, 37, false);
   // sleep.addRecord(2022, 1, 19, 22, 50, 8, 5, true);
