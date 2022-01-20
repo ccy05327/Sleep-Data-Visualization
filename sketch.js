0a12743a41f3debc4af8c8897190f5f446335e5e
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-01-20 10:52:43 +0800

    Update sketch.js
    1/19 sleep

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function setup() {
   sleep.addRecord(2022, 1, 16, 22, 39, 6, 57, false);
   sleep.addRecord(2022, 1, 17, 23, 16, 7, 59, true);
   sleep.addRecord(2022, 1, 18, 23, 8, 7, 42, true);
-  // sleep.addRecord(2022, 1, 19, 22, 50, 8, 5, true);
+  sleep.addRecord(2022, 1, 19, 23, 32, 7, 35, true);
   // sleep.addRecord(2022, 1, 20, 23, 30, 8, 3, true);
   // sleep.addRecord(2022, 1, 21);
   // sleep.addRecord(2022, 1, 22, 0, 41, 7, 1, true);
